7972d53409e1584f4b6251e83486307f905e0f7d
Lee Jinsol <n.solee9@gmail.com>
2024-08-26 11:14:07 +0900

    feat: 회원 서비스 (로그인, 조회, 전체 조회)

---
+++ src/main/java/service/MemberService.java
@@ -0,0 +1,41 @@
+package service;
+
+import hello.hello_spring.domain.Member;
+import repository.MemberRepository;
+import repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 회원 가입
+    public  Long join(Member member) {
+        // 1. null의 가능성이 있으면 Optional로 감싸서 값을 전달
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> { // ifPresent: 값이 있으면 값은 m으로 지정
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        // 2. 1번 코드 정리
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) { // 중복 검증
+        memberRepository.findByName(member.getName())
+                .ifPresent(m-> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
